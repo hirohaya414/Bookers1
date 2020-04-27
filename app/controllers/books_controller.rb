497e2c19010fa7e33f79759f1119c6ab49907e06
vagrant <vagrant@localhost.localdomain>
2020-04-27 08:50:10 +0000

    [Update] サクセスメッセージの表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,6 +10,7 @@ class BooksController < ApplicationController
   def create
   	@book = Book.new(book_params)
   	if @book.save
+      flash[:notice] = "Book was successfully created."
   		redirect_to book_path(@book.id)
   	else
     @books = Book.all
@@ -27,8 +28,10 @@ class BooksController < ApplicationController
 
   def update
   	book = Book.find(params[:id])
-    book.update(book_params)
-  	redirect_to book_path(book.id)
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+  	  redirect_to book_path(book.id)
+    end
   end
 
   def destroy
