5fa8597c6b3de531262ab90611692a70e03d5cee
vagrant <vagrant@localhost.localdomain>
2020-04-27 08:04:36 +0000

    [Update] エラーメッセージの表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,9 +11,10 @@ class BooksController < ApplicationController
   	@book = Book.new(book_params)
   	if @book.save
   		redirect_to book_path(@book.id)
-  	end
-  	@books = Book.all
-  	render :index
+  	else
+    @books = Book.all
+    render :index
+    end
   end
 
   def show
@@ -26,14 +27,16 @@ class BooksController < ApplicationController
 
   def update
   	book = Book.find(params[:id])
-  	book.update(book_params)
+    book.update(book_params)
   	redirect_to book_path(book.id)
   end
 
   def destroy
   	book = Book.find(params[:id])
-  	book.destroy
-  	redirect_to books_path
+  	if book.destroy
+      flash[:notice] = "Book was successfully destroyed."
+  	  redirect_to books_path
+    end
   end
 
   private
